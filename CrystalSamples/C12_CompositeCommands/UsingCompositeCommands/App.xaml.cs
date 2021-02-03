51f8c77dc746a20e29e72e397253746cc2a9b633
jinhuca <jinhuca@gmail.com>
2021-02-03 00:55:45 -0500

    sdw

--- CrystalSamples/C12_CompositeCommands/UsingCompositeCommands/App.xaml.cs
+++ CrystalSamples/C12_CompositeCommands/UsingCompositeCommands/App.xaml.cs
@@ -7,24 +7,21 @@ using UsingCompositeCommands.Views;
 
 namespace UsingCompositeCommands
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : CrystalApplication
-    {
-        protected override Window CreateShell()
-        {
-            return Container.Resolve<MainWindow>();
-        }
+	public partial class App : CrystalApplication
+	{
+		protected override Window CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
 
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
-        {
-            moduleCatalog.AddModule<ModuleA.ModuleAModule>();
-        }
+		protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+		{
+			moduleCatalog.AddModule<ModuleA.ModuleAModule>();
+		}
 
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.RegisterSingleton<IApplicationCommands, ApplicationCommands>();
-        }
-    }
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+			containerRegistry.RegisterSingleton<IApplicationCommands, ApplicationCommands>();
+		}
+	}
 }
