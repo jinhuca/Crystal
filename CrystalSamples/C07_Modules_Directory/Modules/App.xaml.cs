bc85b95fc85a1003554206cff5405fe5c6720ead
jinhuca <jinhuca@gmail.com>
2021-02-02 23:29:17 -0500

    sd

--- CrystalSamples/C07_Modules_Directory/Modules/App.xaml.cs
+++ CrystalSamples/C07_Modules_Directory/Modules/App.xaml.cs
@@ -2,12 +2,6 @@
 using Crystal.Modularity;
 using Crystal.Ioc;
 using Modules.Views;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Modules
