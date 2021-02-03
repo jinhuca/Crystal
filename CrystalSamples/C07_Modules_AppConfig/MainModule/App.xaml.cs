69ba074f359a5fe43970ccee0fb401a9ffef5145
jinhuca <jinhuca@gmail.com>
2021-02-02 22:05:02 -0500

    xc

---
+++ CrystalSamples/C07_Modules_AppConfig/MainModule/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Crystal.Unity;
+using Crystal.Modularity;
+using Crystal.Ioc;
+using System.Windows;
+using MainModule.Views;
+
+namespace MainModule
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : CrystalApplication
+	{
+		protected override Window CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+
+		protected override IModuleCatalog CreateModuleCatalog()
+		{
+			return new ConfigurationModuleCatalog();
+		}
+	}
+}
---
+++ CrystalSamples/C07_Modules_AppConfig/ModuleA/ModuleAModule.cs
@@ -0,0 +1,20 @@
+﻿using Crystal.Ioc;
+using Crystal.Modularity;
+using Crystal.Regions;
+using ModuleA.Views;
+
+namespace ModuleA
+{
+	public class ModuleAModule : IModule
+	{
+		public void OnInitialized(IContainerProvider containerProvider)
+		{
+			var regionManager = containerProvider.Resolve<IRegionManager>();
+			regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+		}
+
+		public void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+		}
+	}
+}
