ace6effa93433dcefe01a280b54b7b56fbd00a32
jinhuca <jinhuca@gmail.com>
2021-02-07 01:37:12 -0500

    19

---
+++ CrystalSamples/C19_NavigationParticipation/ModuleA/ModuleAModule.cs
@@ -0,0 +1,19 @@
+﻿using Crystal.Ioc;
+using Crystal.Modularity;
+using ModuleA.Views;
+
+namespace ModuleA
+{
+	public class ModuleAModule : IModule
+	{
+		public void OnInitialized(IContainerProvider containerProvider)
+		{
+		}
+
+		public void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+			containerRegistry.RegisterForNavigation<ViewA>();
+			containerRegistry.RegisterForNavigation<ViewB>();
+		}
+	}
+}
---
+++ CrystalSamples/C19_NavigationParticipation/NavigationParticipation/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Crystal.Ioc;
+using Crystal.Modularity;
+using Crystal.Unity;
+using NavigationParticipation.Views;
+using System.Windows;
+
+namespace NavigationParticipation
+{
+	public partial class App : CrystalApplication
+	{
+		protected override Window CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+
+		protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+		{
+			moduleCatalog.AddModule<ModuleA.ModuleAModule>();
+		}
+	}
+}
