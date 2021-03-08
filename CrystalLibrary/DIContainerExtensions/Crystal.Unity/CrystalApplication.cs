a84ce4ce8421d27f08171d962fe79a665ed94f44
jinhuca <jinhuca@gmail.com>
2021-03-08 15:40:15 -0500

    sd

--- CrystalLibrary/DIContainerExtensions/Crystal.Unity/CrystalApplication.cs
+++ CrystalLibrary/DIContainerExtensions/Crystal.Unity/CrystalApplication.cs
@@ -6,22 +6,18 @@ namespace Crystal.Unity
 {
 	public abstract class CrystalApplication : CrystalApplicationBase
 	{
-    /// <summary>
-    /// Create a new <see cref="UnityContainerExtension"/> used by Crystal.
-    /// </summary>
-    /// <returns>A new <see cref="UnityContainerExtension"/>.</returns>
-    protected override IContainerExtension CreateContainerExtension()
-    {
-      return new UnityContainerExtension();
-    }
+		/// <summary>
+		/// Create a new <see cref="UnityContainerExtension"/> used by Crystal.
+		/// </summary>
+		/// <returns>A new <see cref="UnityContainerExtension"/>.</returns>
+		protected override IContainerExtension CreateContainerExtension() 
+			=> new UnityContainerExtension();
 
-    /// <summary>
-    /// Registers the <see cref="Type"/>s of the Exceptions that are not considered 
-    /// root exceptions by the <see cref="ExceptionExtensions"/>.
-    /// </summary>
-    protected override void RegisterFrameworkExceptionTypes()
-    {
-      ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ResolutionFailedException));
-    }
-  }
+		/// <summary>
+		/// Registers the <see cref="Type"/>s of the Exceptions that are not considered 
+		/// root exceptions by the <see cref="ExceptionExtensions"/>.
+		/// </summary>
+		protected override void RegisterFrameworkExceptionTypes() 
+			=> ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ResolutionFailedException));
+	}
 }
--- CrystalLibrary/DIContainerExtensions/Crystal.Unity/IocExtensions.cs
+++ CrystalLibrary/DIContainerExtensions/Crystal.Unity/IocExtensions.cs
@@ -13,13 +13,15 @@ namespace Crystal.Unity
 		/// </summary>
 		/// <param name="containerProvider">The current <see cref="IContainerProvider"/></param>
 		/// <returns>The underlying <see cref="IUnityContainer"/></returns>
-		public static IUnityContainer GetContainer(this IContainerProvider containerProvider) => ((IContainerExtension<IUnityContainer>)containerProvider).Instance;
+		public static IUnityContainer GetContainer(this IContainerProvider containerProvider) 
+			=> ((IContainerExtension<IUnityContainer>)containerProvider).Instance;
 
 		/// <summary>
 		/// Gets the <see cref="IUnityContainer" /> from the <see cref="IContainerProvider" />
 		/// </summary>
 		/// <param name="containerRegistry">The current <see cref="IContainerRegistry" /></param>
 		/// <returns>The underlying <see cref="IUnityContainer" /></returns>
-		public static IUnityContainer GetContainer(this IContainerRegistry containerRegistry) => ((IContainerExtension<IUnityContainer>)containerRegistry).Instance;
+		public static IUnityContainer GetContainer(this IContainerRegistry containerRegistry) 
+			=> ((IContainerExtension<IUnityContainer>)containerRegistry).Instance;
 	}
 }
