4c818e41e045d345b959ce51880ee05df7a1c1ad
jinhuca <jinhuca@gmail.com>
2021-04-13 14:55:50 -0400

    rt3

---
+++ Samples/C05_Views/C0506_CustomRegistration/ViewModelLocator/ViewModels/CustomViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Crystal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelLocator.ViewModels
+{
+	public class CustomViewModel : BindableBase
+  {
+    private string _title = "Custom ViewModel Application";
+    public string Title
+    {
+      get { return _title; }
+      set { SetProperty(ref _title, value); }
+    }
+
+    public CustomViewModel()
+    {
+
+    }
+  }
+}
---
+++ Samples/C06_Commands/C0601_DelegateCommands/C0601UsingDelegateCommand/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using C0601UsingDelegateCommand.Views;
+using Crystal;
+using Crystal.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace C0601UsingDelegateCommand
+{
+	public partial class App : CrystalApplication
+	{
+		protected override Window CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
+	}
+}
