179d535cdb8c65e876bde40838961f3a3784274f
Jin Hu <Jin.Hu@bsci.com>
2021-11-01 15:39:02 -0400

    b

---
+++ Tutorials/S00_Bootstrapper/BootstrapperShell/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using BootstrapperShell.Views;
+using Crystal;
+using Crystal.Desktop;
+
+namespace BootstrapperShell
+{
+	public class Bootstrapper : CrystalBootstrapper
+	{
+		protected override DependencyObject CreateShell() => Container.Resolve<MainWindow>();
+	}
+}
