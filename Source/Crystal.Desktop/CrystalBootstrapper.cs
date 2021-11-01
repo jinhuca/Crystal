61681ae4e087b0edac020e0595c8bef953356256
jinhuca <jinhuca@gmail.com>
2021-11-01 15:25:41 -0400

    Bootstrapper

--- Source/Crystal.Desktop/CrystalBootstrapper.cs
+++ Source/Crystal.Desktop/CrystalBootstrapper.cs
@@ -3,32 +3,26 @@
 namespace Crystal.Desktop
 {
 	/// <summary>
-	/// Base bootstrapper class that uses <see cref="DryIocContainerExtension"/> as it's container.
+	/// Base bootstrapper class that uses <see cref="ContainerExtension"/> as it's container.
 	/// </summary>
 	public abstract class CrystalBootstrapper : CrystalBootstrapperBase
-    {
-        /// <summary>
-        /// Create <see cref="Rules" /> to alter behavior of <see cref="IContainer" />
-        /// </summary>
-        /// <returns>An instance of <see cref="Rules" /></returns>
-        protected virtual Rules CreateContainerRules() => ContainerExtension.DefaultRules;
+	{
+		/// <summary>
+		/// Create <see cref="Rules" /> to alter behavior of <see cref="IContainer" />
+		/// </summary>
+		/// <returns>An instance of <see cref="Rules" /></returns>
+		protected virtual Rules CreateContainerRules() => ContainerExtension.DefaultRules;
 
-        /// <summary>
-        /// Create a new <see cref="DryIocContainerExtension"/> used by Crystal.
-        /// </summary>
-        /// <returns>A new <see cref="DryIocContainerExtension"/>.</returns>
-        protected override IContainerExtension CreateContainerExtension()
-        {
-            return new ContainerExtension(new Container(CreateContainerRules()));
-        }
+		/// <summary>
+		/// Create a new <see cref="ContainerExtension"/> used by Crystal.
+		/// </summary>
+		/// <returns>A new <see cref="ContainerExtension"/>.</returns>
+		protected override IContainerExtension CreateContainerExtension() => new ContainerExtension(new Container(CreateContainerRules()));
 
-        /// <summary>
-        /// Registers the <see cref="Type"/>s of the Exceptions that are not considered 
-        /// root exceptions by the <see cref="ExceptionExtensions"/>.
-        /// </summary>
-        protected override void RegisterFrameworkExceptionTypes()
-        {
-            ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
-        }
-    }
+		/// <summary>
+		/// Registers the <see cref="Type"/>s of the Exceptions that are not considered 
+		/// root exceptions by the <see cref="ExceptionExtensions"/>.
+		/// </summary>
+		protected override void RegisterFrameworkExceptionTypes() => ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
+	}
 }
