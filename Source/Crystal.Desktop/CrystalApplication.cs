7a3239f2119e87c1e039d45c5e9f888b7aa31c0e
Jin Hu <jinhuca@gmail.com>
2021-11-02 00:39:10 -0400

    er

--- Source/Crystal.Desktop/CrystalApplication.cs
+++ Source/Crystal.Desktop/CrystalApplication.cs
@@ -14,14 +14,12 @@ namespace Crystal
 		/// Create a new <see cref="ContainerExtension"/> used by Crystal.
 		/// </summary>
 		/// <returns>A new <see cref="ContainerExtension"/>.</returns>
-		protected override IContainerExtension CreateContainerExtension()
-			=> new ContainerExtension(new Container(CreateContainerRules()));
+		protected override IContainerExtension CreateContainerExtension() => new ContainerExtension(new Container(CreateContainerRules()));
 
 		/// <summary>
 		/// Registers the <see cref="Type"/>s of the Exceptions that are not considered 
 		/// root exceptions by the <see cref="ExceptionExtensions"/>.
 		/// </summary>
-		protected override void RegisterFrameworkExceptionTypes()
-			=> ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
+		protected override void RegisterFrameworkExceptionTypes() => ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
 	}
 }
