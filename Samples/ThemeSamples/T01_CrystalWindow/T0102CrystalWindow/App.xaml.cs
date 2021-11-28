3458aa83cdaaa9f1e1f0be67c0205dfad115c516
jinhuca <jinhuca@gmail.com>
2021-11-27 22:43:35 -0500

    45

--- Samples/ThemeSamples/T01_CrystalWindow/T0102CrystalWindow/App.xaml.cs
+++ Samples/ThemeSamples/T01_CrystalWindow/T0102CrystalWindow/App.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace T0102CrystalWindow
+﻿namespace T0102CrystalWindow
 {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
-  public partial class App : Application
+  public partial class App
   {
   }
 }
