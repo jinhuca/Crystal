61bcb77cbfbfb8b19528f99ef99f9c020c52ac55
Jin Hu <jinhuca@gmail.com>
2026-07-24 19:03:57 -0400

    1008

---
+++ TryIt/TryIt01_PInvoke/T01_Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TryIt01_PInvoke; 
+internal class T01_Program {
+  static void Main(string[] args) {
+    var monitor = new CpuMemoryMonitor();
+
+    while (true) {
+      var sample = monitor.GetSample();
+
+      Console.WriteLine(
+          $"CPU={sample.CpuUsagePercent:F1}%  " +
+          $"RAM={sample.MemoryUsagePercent:F1}%");
+
+      Thread.Sleep(1000);
+    }
+  }
+}
