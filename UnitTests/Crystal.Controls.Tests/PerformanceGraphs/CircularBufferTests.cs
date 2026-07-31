35fdaed4114e9b67c2558698cbcf30b80079962c
Jin Hu <jinhuca@gmail.com>
2026-07-31 17:46:24 -0400

    tests

---
+++ UnitTests/Crystal.Controls.Tests/PerformanceGraphs/CircularBufferTests.cs
@@ -0,0 +1,99 @@
+using Crystal.Controls.PerformanceGraphs.Buffers;
+using Xunit;
+
+namespace Crystal.Controls.Tests.PerformanceGraphs;
+
+public class CircularBufferTests {
+  [Fact]
+  public void Constructor_NonPositiveCapacity_Throws() {
+    Assert.Throws<ArgumentOutOfRangeException>(() => new CircularBuffer<int>(0));
+    Assert.Throws<ArgumentOutOfRangeException>(() => new CircularBuffer<int>(-1));
+  }
+
+  [Fact]
+  public void NewBuffer_IsEmpty() {
+    var buffer = new CircularBuffer<int>(3);
+
+    Assert.Equal(3, buffer.Capacity);
+    Assert.Equal(0, buffer.Count);
+  }
+
+  [Fact]
+  public void Add_BelowCapacity_AppendsInOrder() {
+    var buffer = new CircularBuffer<int>(3);
+
+    buffer.Add(10);
+    buffer.Add(20);
+
+    Assert.Equal(2, buffer.Count);
+    Assert.Equal(10, buffer[0]);
+    Assert.Equal(20, buffer[1]);
+  }
+
+  [Fact]
+  public void Add_BeyondCapacity_EvictsOldestAndKeepsOrder() {
+    var buffer = new CircularBuffer<int>(3);
+
+    buffer.Add(1);
+    buffer.Add(2);
+    buffer.Add(3);
+    buffer.Add(4); // evicts 1
+    buffer.Add(5); // evicts 2
+
+    Assert.Equal(3, buffer.Count);
+    Assert.Equal(3, buffer[0]); // oldest
+    Assert.Equal(4, buffer[1]);
+    Assert.Equal(5, buffer[2]); // newest
+  }
+
+  [Fact]
+  public void Indexer_IsOldestFirst_AfterManyWraps() {
+    var buffer = new CircularBuffer<int>(4);
+    for (int i = 0; i < 20; i++) buffer.Add(i);
+
+    // The last four values added: 16,17,18,19.
+    Assert.Equal(16, buffer[0]);
+    Assert.Equal(17, buffer[1]);
+    Assert.Equal(18, buffer[2]);
+    Assert.Equal(19, buffer[3]);
+  }
+
+  [Theory]
+  [InlineData(-1)]
+  [InlineData(2)]
+  public void Indexer_OutOfRange_Throws(int index) {
+    var buffer = new CircularBuffer<int>(3);
+    buffer.Add(1);
+    buffer.Add(2);
+
+    Assert.Throws<ArgumentOutOfRangeException>(() => buffer[index]);
+  }
+
+  [Fact]
+  public void Clear_ResetsCount_AndReusesCapacity() {
+    var buffer = new CircularBuffer<int>(3);
+    buffer.Add(1);
+    buffer.Add(2);
+
+    buffer.Clear();
+
+    Assert.Equal(0, buffer.Count);
+    Assert.Throws<ArgumentOutOfRangeException>(() => buffer[0]);
+
+    buffer.Add(99);
+    Assert.Equal(1, buffer.Count);
+    Assert.Equal(99, buffer[0]);
+  }
+
+  [Fact]
+  public void CapacityOne_AlwaysHoldsMostRecentValue() {
+    var buffer = new CircularBuffer<int>(1);
+
+    buffer.Add(1);
+    buffer.Add(2);
+    buffer.Add(3);
+
+    Assert.Equal(1, buffer.Count);
+    Assert.Equal(3, buffer[0]);
+  }
+}
---
+++ UnitTests/Crystal.Controls.Tests/PerformanceGraphs/ValueFormatConverterTests.cs
@@ -0,0 +1,57 @@
+using System.Globalization;
+using Crystal.Controls.PerformanceGraphs.Controls;
+using Xunit;
+
+namespace Crystal.Controls.Tests.PerformanceGraphs;
+
+public class ValueFormatConverterTests {
+  private static readonly ValueFormatConverter Converter = new();
+
+  [Fact]
+  public void Convert_AppliesPercentFormat() {
+    object result = Converter.Convert(new object[] { 100.0, "{0}%" }, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal("100%", result);
+  }
+
+  [Fact]
+  public void Convert_AppliesVoltFormat() {
+    object result = Converter.Convert(new object[] { 3.0, "{0}V" }, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal("3V", result);
+  }
+
+  [Fact]
+  public void Convert_MissingFormat_DefaultsToPlainValue() {
+    object result = Converter.Convert(new object[] { 60, null! }, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal("60", result);
+  }
+
+  [Fact]
+  public void Convert_TooFewValues_ReturnsEmptyString() {
+    object result = Converter.Convert(new object[] { 1 }, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal(string.Empty, result);
+  }
+
+  [Fact]
+  public void Convert_NullValues_ReturnsEmptyString() {
+    object result = Converter.Convert(null!, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal(string.Empty, result);
+  }
+
+  [Fact]
+  public void Convert_InvalidFormat_FallsBackToValueToString() {
+    object result = Converter.Convert(new object[] { 7, "{0" }, typeof(string), null!, CultureInfo.InvariantCulture);
+
+    Assert.Equal("7", result);
+  }
+
+  [Fact]
+  public void ConvertBack_Throws() {
+    Assert.Throws<NotSupportedException>(() =>
+        Converter.ConvertBack("x", new[] { typeof(double) }, null!, CultureInfo.InvariantCulture));
+  }
+}
