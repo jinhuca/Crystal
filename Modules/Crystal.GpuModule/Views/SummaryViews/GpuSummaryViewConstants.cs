6bbf4a33f61ce09d9e73a9997369ee836914e191
Jin Hu <jinhuca@gmail.com>
2026-09-05 21:07:06 -0400

    32122

---
+++ Modules/Crystal.GpuModule/Views/SummaryViews/GpuSummaryViewConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+namespace Crystal.GpuModule.Views.SummaryViews;
+
+//internal sealed class GraphWidthExtension : MarkupExtension {
+//  public override object ProvideValue(IServiceProvider serviceProvider) =>
+//      GpuSummaryViewConstants.GraphWidth;
+//}
+
+internal static class GpuSummaryViewConstants {
+  public const double GraphWidth = 200d;
+  public const double UtilizationGraphWidth = 240d;
+}
