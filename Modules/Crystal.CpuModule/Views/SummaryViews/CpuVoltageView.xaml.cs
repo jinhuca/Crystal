dc6004e35ec5eb4096afa237158894586b3ae4e0
Jin Hu <jinhuca@gmail.com>
2026-09-12 16:18:48 -0400

    321

--- Modules/Crystal.CpuModule/Views/SummaryViews/CpuVoltageView.xaml.cs
+++ Modules/Crystal.CpuModule/Views/SummaryViews/CpuVoltageView.xaml.cs
@@ -1,3 +1,6 @@
+using Crystal.Controls.PerformanceGraphs;
+using Crystal.CpuModule.ViewModels.Interfaces;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Crystal.CpuModule.Views.SummaryViews;
@@ -9,5 +12,17 @@ namespace Crystal.CpuModule.Views.SummaryViews;
 public partial class CpuVoltageView : UserControl {
   public CpuVoltageView() {
     InitializeComponent();
+  }
+
+  /// <summary>
+  /// Self-registers the bottom history strip with the inherited SensorsViewModel so it is fed on
+  /// every poll, keyed by its <c>GraphIdentity.Id</c> ("Cpu.Voltage"). Mirrors the Power tile.
+  /// </summary>
+  private void OnGraphLoaded(object sender, RoutedEventArgs e) {
+    if (sender is ISingleSeriesGraph graph
+        && sender is FrameworkElement { DataContext: ICpuSensorViewModel vm } fe
+        && GraphIdentity.GetId(fe) is { } id) {
+      vm.AttachGraph(id, graph);
+    }
   }
 }
